cb7cf40313714e7a48b72359f1d5f162756030db
Mary <mekp@hawaii.edu>
2017-02-27 15:55:27 -1000

    Task 3: Collections, Forms and events, update and remove

---
+++ app/import/ui/task.js
@@ -0,0 +1,21 @@
+import { Mongo } from 'meteor/mongo';
+
+export const Tasks = new Mongo.Collection('tasks');
+
+import { Template } from 'meteor/templating';
+
+import { Tasks } from '../api/tasks.js';
+
+import './task.html';
+
+Template.task.events({
+  'click .toggle-checked'() {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this._id, {
+      $set: { checked: ! this.checked },
+    });
+  },
+  'click .delete'() {
+    Tasks.remove(this._id);
+  },
+});
